726600a5d169c40e6030d6e024443e116973e67c
Imtaek Hong <imtaekh@gmail.com>
2015-07-19 00:11:19 -0700

    patch problem

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ class MyRestaurantApp < Sinatra::Base
 
   get "/menu/all" do
     @menu = Menu.all
+    erb :menu_all
   end
 
   get "/menu/new" do
@@ -12,7 +13,28 @@ class MyRestaurantApp < Sinatra::Base
     erb :menu_new
   end
 
-  get "/order" do
+  post "/menu/new" do
+    Menu.create(params[:menu])
+    redirect "menu/all"
+  end
+
+  patch "/menu/edit/:id" do
+    @menu = User.find_by(id: params[:id])
+    @menu.update(params[:menu])
+    redirect "menu/all"
+  end
+
+  get "/menu/edit/:id" do
+    @menu = Menu.find_by(id: params[:id])
+    erb :menu_edit
+  end
+
+  post "/menu/edit" do
+    Menu.create(params[:menu])
+    redirect "menu/all"
+  end
+
+  get "/order/all" do
     @order = Order.all
   end
 end
